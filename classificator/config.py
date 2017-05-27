2332b0309d6be668389463c92232995772e1d017
vorobyev <nikgeneralmail@gmail.com>
2017-05-27 14:35:33 +0300

    create new model

--- classificator/config.py
+++ classificator/config.py
@@ -6,11 +6,11 @@ def getParams():
 		'embedding_dims' : 100,
 		'filters' : 200,
 		'kernel_size' : 5,
-		'hidden_dims' : 250,
+		'hidden_dims' : 100,
 		'epochs' : 10,
 		'model_dict': 'tokenDictionary.npy',
 		'first_model': 'myModel.h5',
-		'second_model': '',
+		'second_model': 'myModel_1.h5',
 		'thirty_model':'',
 		'corpus': 'NEW_DATA.json'
 	}
